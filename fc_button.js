11b61f1479897e38dca5315a57dfa22a401a4486
kisslab69 <42703934+kisslab69@users.noreply.github.com>
2018-11-04 20:26:07 +0100

    Update fc_button.js

--- fc_button.js
+++ fc_button.js
@@ -638,7 +638,7 @@ function updateTimers() {
     }
 
     height = $('#backgroundLeftCanvas').height() - 140;
-    drawCircles(t_draw, 20, height);
+   // drawCircles(t_draw, 20, height);
 }
 
 function FCMenu() {
