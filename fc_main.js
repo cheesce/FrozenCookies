68b7b774175783c9c3c48055d5652ee5073eeee2
kisslab69 <42703934+kisslab69@users.noreply.github.com>
2018-11-08 02:10:33 +0100

    Update fc_main.js

--- fc_main.js
+++ fc_main.js
@@ -375,7 +375,7 @@ function getProbabilityList(listType) { //ok
 }
 
 function getProbabilityModifiers(listType) { //ok spawnrate modifier
-   return (eval('me='+Game.shimmerTypes['golden'].getTimeMod.toString().replace(/me\.wrath/,Game.elderWrath))(me,1))/(Game.fps*60);
+   return (eval('me='+Game.shimmerTypes[listType].getTimeMod.toString().replace(/me\.wrath/,Game.elderWrath))(me,1))/(Game.fps*60);
 }
 
 function probabilitySpan(listType, start, endProbability) { //ok
