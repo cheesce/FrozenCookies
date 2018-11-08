dddae73535a14d72094ddfa5446be6dda32077be
kisslab69 <42703934+kisslab69@users.noreply.github.com>
2018-11-08 01:18:57 +0100

    more updates

--- fc_main.js
+++ fc_main.js
@@ -344,33 +344,47 @@ function autoCast() {
     }
 }
 
-//functions for buying stuff
-function getProbabilityList(listType) {
+//calculate Probabilities for spawn of golden cookies and reindeers 
+var cumulativeProbabilityList = {
+    golden : [1, 0.95, 0.5, 0.475, 0.25, 0.2375].reduce(function(r,x) {
+        r[x] = generateProbabilities(x, 5 * 60 * Game.fps, 3);
+        return r;
+    }, {}),
+    reindeer : [1, 0.5].reduce(function(r,x) {
+        r[x] = generateProbabilities(x, 3 * 60 * Game.fps, 2);
+        return r;
+    }, {})
+};
+
+function generateProbabilities(upgradeMult, minBase, maxMult) { //ok
+    var cumProb = [];
+    var remainingProbability = 1;
+    var minTime = minBase * upgradeMult;
+    var maxTime = maxMult * minTime;
+    var spanTime = maxTime - minTime;
+    for (var i=0; i<maxTime; i++) {
+        var thisFrame = remainingProbability * Math.pow(Math.max(0,(i-minTime)/spanTime),5);
+        remainingProbability -= thisFrame;
+        cumProb.push(1 - remainingProbability);
+    }
+    return cumProb;
+}
+
+function getProbabilityList(listType) { //ok
     return cumulativeProbabilityList[listType][getProbabilityModifiers(listType)];
 }
 
-function getProbabilityModifiers(listType) {
-    var result;
-    switch (listType) {
-        case "golden":
-            result = (Game.Has('Lucky day') ? 0.5 : 1) * (Game.Has('Serendipity') ? 0.5 : 1) * (Game.Has('Golden goose egg') ? 0.95 : 1);
-            break;
-        case "reindeer":
-            result = Game.Has('Reindeer baking grounds') ? 0.5 : 1;
-            break;
-    }
-    return result;
+function getProbabilityModifiers(listType) { //ok spawnrate modifier
+    return Game.shimmerTypes[listType].getTimeMod(null,1)/(Game.fps*60);
 }
 
-function cumulativeProbability(listType, start, stop) {
-    return 1 - ((1 - getProbabilityList(listType)[stop]) / (1 - getProbabilityList(listType)[start]));
-}
-
-function probabilitySpan(listType, start, endProbability) {
+function probabilitySpan(listType, start, endProbability) { //ok
     var startProbability = getProbabilityList(listType)[start];
     return _.sortedIndex(getProbabilityList(listType), (startProbability + endProbability - startProbability * endProbability));
 }
 
+
+// math
 function clickBuffBonus() {
     var ret = 1
     for (var i in Game.buffs) {
